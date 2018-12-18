8c884702c5a654a60cab6de86a397239c87dfe5f
InhyeJeong <inhyejeong59@gmail.com>
2018-12-18 15:48:32 +0900

    uml diagram / contactController 수정

--- MyBlog/src/main/java/edu/iot/myblog/controller/ContactController.java
+++ MyBlog/src/main/java/edu/iot/myblog/controller/ContactController.java
@@ -1,13 +1,10 @@
 package edu.iot.myblog.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import edu.iot.common.dao.Email;
-import edu.iot.common.dao.EmailSender;
 
 @Controller
 @RequestMapping("/contact")
@@ -16,14 +13,14 @@ public class ContactController {
 //	@Autowired
 //	private EmailSender emailSender;
 	
-	/** 목록보기 /list */
+	/** 메일 전송 form */
 	@RequestMapping(value = "mailForm", method=RequestMethod.GET)
 	public void mailForm(Model model) throws Exception {
 
 		
 	}
 	
-	/** 목록보기 /list */
+	/** 전송 성공시 jsp로 */
 	@RequestMapping(value = "mailSend", method=RequestMethod.POST)
 	public void mailSend(Model model) throws Exception {
 
